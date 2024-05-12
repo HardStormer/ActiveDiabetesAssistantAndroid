6d0a906d90c6d845d017f353619b7b44a4afdca0
guzeev.mikhail <guzeev.md@gmail.com>
2024-05-13 03:41:49 +0400

    feat: files research

--- ActiveDiabetesAssistantAndroidApp/app/src/main/java/com/example/activediabetesassistantandroidapp/ui/slideshow/PersonInfoUpdateFragment.kt
+++ ActiveDiabetesAssistantAndroidApp/app/src/main/java/com/example/activediabetesassistantandroidapp/ui/slideshow/PersonInfoUpdateFragment.kt
@@ -1,4 +1,4 @@
-package com.example.activediabetesassistantandroidapp.ui.gallery
+package com.example.activediabetesassistantandroidapp.ui.slideshow
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.activediabetesassistantandroidapp.databinding.FragmentGalleryBinding
+import com.example.activediabetesassistantandroidapp.databinding.FragmentPersonInfoUpdateBinding
 
-class GalleryFragment : Fragment() {
+class PersonInfoUpdateFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentPersonInfoUpdateBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val personInfoUpdateViewModel =
+            ViewModelProvider(this).get(PersonInfoUpdateViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentPersonInfoUpdateBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textSlideshow
+        personInfoUpdateViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
