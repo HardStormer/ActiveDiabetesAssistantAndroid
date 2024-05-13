e2006f71f1be00e57edee04f0bb45239c395f824
guzeev.mikhail <guzeev.md@gmail.com>
2024-05-13 22:03:28 +0400

    fix: new project

---
+++ ActiveDiabetesAssistantAndroid/app/src/main/java/ru/guzeevmd/activediabetesassistant/MainActivity.kt
@@ -0,0 +1,48 @@
+package ru.guzeevmd.activediabetesassistant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import ru.guzeevmd.activediabetesassistant.ui.theme.ActiveDiabetesAssistantAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ActiveDiabetesAssistantAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "232432423",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActiveDiabetesAssistantAndroidTheme {
+        Greeting("Android")
+    }
+}
