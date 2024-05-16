3c9bbeb77059d47aeb0d7adc69412a09c90944a1
guzeev.mikhail <guzeev.md@gmail.com>
2024-05-16 04:28:28 +0400

    fix: person card refactor

--- ActiveDiabetesAssistantAndroid/app/src/main/java/ru/guzeevmd/activediabetesassistant/cards/PersonInfoCard.kt
+++ ActiveDiabetesAssistantAndroid/app/src/main/java/ru/guzeevmd/activediabetesassistant/cards/PersonInfoCard.kt
@@ -14,9 +14,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -24,8 +27,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -39,27 +44,28 @@ import ru.guzeevmd.activediabetesassistant.data.models.PersonInfoViewModel
 import ru.guzeevmd.activediabetesassistant.data.models.Sex
 import ru.guzeevmd.activediabetesassistant.data.models.UpdateMyPersonInfoCommand
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun PersonInfoCard(
     viewModel: AuthViewModel,
     personInfo: PersonInfoViewModel,
     paddingValues: PaddingValues,
     snackbarHostState: SnackbarHostState,
-    authToken: String) {
+    authToken: String
+) {
     var name by remember { mutableStateOf(personInfo.name ?: "") }
     var age by remember { mutableStateOf(personInfo.age.toString()) }
     var sex by remember { mutableIntStateOf(personInfo.sex) }
     var diabetesType by remember { mutableIntStateOf(personInfo.diabetesType) }
     var isEditable by remember { mutableStateOf(false) }
-    val coroutineScope = rememberCoroutineScope()
     var isLoading by remember { mutableStateOf(false) }
-
+    val coroutineScope = rememberCoroutineScope()
     val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(paddingValues)
             .padding(16.dp)
     ) {
         TextField(
@@ -68,6 +74,9 @@ fun PersonInfoCard(
             label = { Text("Имя") },
             modifier = Modifier.fillMaxWidth(),
             readOnly = !isEditable,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent
+            )
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -79,6 +88,9 @@ fun PersonInfoCard(
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
             readOnly = !isEditable,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent
+            )
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -96,7 +108,8 @@ fun PersonInfoCard(
                     text = Sex.fromInt(sex).name,
                     modifier = Modifier
                         .padding(16.dp)
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyMedium
                 )
 
                 DropdownMenu(
@@ -119,12 +132,14 @@ fun PersonInfoCard(
                 onValueChange = {},
                 label = { Text("Пол") },
                 modifier = Modifier.fillMaxWidth(),
-                readOnly = true
+                readOnly = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent
+                )
             )
         }
 
         Spacer(modifier = Modifier.height(8.dp))
-
 
         if (isEditable) {
             var expanded by remember { mutableStateOf(false) }
@@ -139,7 +154,8 @@ fun PersonInfoCard(
                     text = DiabetesType.fromInt(diabetesType).name,
                     modifier = Modifier
                         .padding(16.dp)
-                        .fillMaxWidth()
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyMedium
                 )
 
                 DropdownMenu(
@@ -162,67 +178,70 @@ fun PersonInfoCard(
                 onValueChange = {},
                 label = { Text("Тип диабета") },
                 modifier = Modifier.fillMaxWidth(),
-                readOnly = true
+                readOnly = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent
+                )
             )
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         if (isLoading) {
-            CircularProgressIndicator()
-        }
-        if (isEditable) {
-            Button(
-                onClick = {
-                    keyboardController?.hide()
-                    coroutineScope.launch {
-                        isLoading = true
-                        // Handle save action
-                        val command = UpdateMyPersonInfoCommand(
-                            name = name,
-                            age = age.toInt(),
-                            sex = sex,
-                            diabetesType = diabetesType.toInt()
-                        )
-                        val client = DiabetesAssistantApiClient(authToken)
-                        val res = client.updatePersonInfo(command)
-
-                        isLoading = false
-                        isEditable = false
-                        if (res.status.isSuccess()) {
-                            snackbarHostState.showSnackbar(
-                                message = "Данные сохранены"
-                            )
-                        } else {
-                            val bodyText = res.bodyAsText()
-                            val status = res.status.toString()
-                            snackbarHostState.showSnackbar(
-                                message = "$status $bodyText"
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Сохранить")
+            Column {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .padding(vertical = 20.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
             }
         } else {
-            Button(
-                onClick = {
-                    isEditable = true // Enable editing
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Обновить")
-            }
-            Spacer(Modifier.weight(1f))
-            Button(
-                onClick = {
-                    viewModel.logoutUser()
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("Выйти")
+            if (isEditable) {
+                Button(
+                    onClick = {
+                        keyboardController?.hide()
+                        coroutineScope.launch {
+                            isLoading = true
+                            val command = UpdateMyPersonInfoCommand(
+                                name = name,
+                                age = age.toInt(),
+                                sex = sex,
+                                diabetesType = diabetesType.toInt()
+                            )
+                            val client = DiabetesAssistantApiClient(authToken)
+                            val res = client.updatePersonInfo(command)
+
+                            isLoading = false
+                            isEditable = false
+                            if (res.status.isSuccess()) {
+                                snackbarHostState.showSnackbar("Данные сохранены")
+                            } else {
+                                snackbarHostState.showSnackbar("${res.status} ${res.bodyAsText()}")
+                            }
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Сохранить")
+                }
+            } else {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Button(
+                        onClick = { isEditable = true },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Обновить")
+                    }
+
+
+                    Spacer(Modifier.weight(1f))
+                    Button(
+                        onClick = { viewModel.logoutUser() },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text("Выйти")
+                    }
+                }
             }
         }
     }
--- ActiveDiabetesAssistantAndroid/app/src/main/java/ru/guzeevmd/activediabetesassistant/screens/ProfileScreen.kt
+++ ActiveDiabetesAssistantAndroid/app/src/main/java/ru/guzeevmd/activediabetesassistant/screens/ProfileScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -25,6 +26,7 @@ import ru.guzeevmd.activediabetesassistant.data.client.DiabetesAssistantApiClien
 import ru.guzeevmd.activediabetesassistant.data.models.PersonInfoViewModel
 import ru.guzeevmd.activediabetesassistant.ui.theme.NavigationBarMediumTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     navController: NavController,
